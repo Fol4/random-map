895a327f810df1250d40a31c2196eeee08665e49
Fol4 <ana17110144@gmail.com>
2019-01-23 14:31:53 +0300

    TEST 2

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 import pygame
 
-'init'
+'INIT'
 
 pygame.init()
 pygame.display.init()
+pygame.font.init()
 
-'constant'
+'CONSTANT'
 
-#delay
+# DELAY
 delay = 30
 
-#runloop
+# RUN LOOP
 GameRun = True
 
-#window
+# WINDOW
 WinWidth , WinHeight = 1280, 1024
 window = pygame.display.set_mode((WinWidth, WinHeight))
 
-#me
+# ME
 MeWidth , MeHeight = WinWidth//100, WinHeight//100
 MeX , MeY = WinWidth//2, WinHeight//2
+x , y = MeX, MeY
 me = pygame.Surface((MeWidth, MeHeight))
 me.fill((63,81,181))
 MeSpeed = 5
 
-#TEST
+# TEXT
+FontXY = pygame.font.Font(None , 22)
+FontX, FontY = 0, 0
+TextCoordinates = FontXY.render('x : ' + str(MeX) + ' , y : ' + str(MeY) , True , (0,0,0))
+
+
+# TEST
 TestWidth , TestHeight = WinWidth//100, WinHeight//100
 TestX , TestY = 300, 300
 test = pygame.Surface((TestWidth , TestHeight))
 test.fill((213,0,0))
+test2 = pygame.Surface((TestWidth, TestHeight))
+test2.fill((221,44,0))
+Test2X , Test2Y = -TestX, -TestY
 
-# def drawMe():
+# def drawTree():
 
 
-
-
-'mainloop'
+'MAINLOOP'
 
 while GameRun:
     pygame.display.update()
@@ -43,6 +52,8 @@ while GameRun:
     window.fill((46,125,50))
     window.blit(me, (MeX, MeY))
     window.blit(test, (TestX, TestY))
+    window.blit(TextCoordinates , (FontX, FontY))
+    window.blit(test2 , (Test2X, Test2Y))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GameRun = False
@@ -50,12 +61,24 @@ while GameRun:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
+        y += MeSpeed
+        TextCoordinates = FontXY.render('x : ' + str(x) + ' , y : ' + str(y) , True , (0,0,0))
+        Test2Y += MeSpeed
         TestY += MeSpeed
     if keys[pygame.K_d]:
+        x -= MeSpeed
+        TextCoordinates = FontXY.render('x : ' + str(x) + ' , y : ' + str(y), True, (0, 0, 0))
+        Test2X -= MeSpeed
         TestX -= MeSpeed
     if keys[pygame.K_s]:
+        y -= MeSpeed
+        TextCoordinates = FontXY.render('x : ' + str(x) + ' , y : ' + str(y), True, (0, 0, 0))
+        Test2Y -= MeSpeed
         TestY -= MeSpeed
     if keys[pygame.K_a]:
+        x += MeSpeed
+        TextCoordinates = FontXY.render('x : ' + str(x) + ' , y : ' + str(y), True, (0, 0, 0))
+        Test2X += MeSpeed
         TestX += MeSpeed
 
 
